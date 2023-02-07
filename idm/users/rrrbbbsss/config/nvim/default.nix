874eb57d752ed1cc58e4e0c233a9f95a1f929d16
Royce Strange <rrrbbbsss@ryzst.net>
2023-02-07 13:58:10 -0600

    clean up neovim config

--- idm/users/rrrbbbsss/config/nvim/default.nix
+++ idm/users/rrrbbbsss/config/nvim/default.nix
@@ -3,30 +3,24 @@
   #eventually build out neovim...
 
   home.packages = with pkgs; [
-      # gui
-      neovide
+    neovide
   ];
 
   programs.neovim = {
     enable = true;
-    #defaultEditor = true;
+    #defaultEditor = true; todo: wait for this in stable
     viAlias = true;
     vimAlias = true;
     plugins = with pkgs.vimPlugins; [
       {
-        # for direnv integration
         plugin = direnv-vim;
         type = "lua";
-        config = "";
       }
       {
         plugin = telescope-nvim;
         type = "lua";
-        config = ''
-          '';
       }
       {
-        # for parsing/syntax highlighting
         plugin = nvim-treesitter.withAllGrammars;
         type = "lua";
       }
@@ -38,67 +32,18 @@
       {
         plugin = nvim-web-devicons;
         type = "lua";
-        config = ''
-          require("nvim-web-devicons").setup()
-        '';
+        config = builtins.readFile ./web-devicons.lua;
       }
       {
-        # git
-        plugin = neogit;
-        type = "lua";
-        config = ''
-          local neogit = require('neogit')
-          neogit.setup {}
-        '';
-      }
-      {
-        plugin = plenary-nvim;
-        type = "lua";
-        config = "";
-      }
-      {
-        # for side tree
         plugin = nvim-tree-lua;
         type = "lua";
-        config = ''
-          vim.g.loaded_netrw = 1
-          vim.g.loaded_netrwPlugin = 1
-
-          require("nvim-tree").setup()
-        '';
+        config = builtins.readFile ./nvim-tree.lua;
       }
       {
-        plugin = nvim-web-devicons;
-        type = "lua";
-        config = ''
-          require("nvim-web-devicons").setup()
-        '';
-      }
-      {
-        # for orgmode
         plugin = orgmode;
         type = "lua";
-        config = "";
       }
     ];
-    extraConfig = ''
-      set termguicolors
-      set showtabline=2
-      if exists("g:neovide")
-        set guifont=Hack\ Nerd\ Font:h10
-        let g:neovide_cursor_animation_length=0
-        let g:neovide_scale_factor=1.0
-        function! ChangeScaleFactor(delta)
-          if a:delta == 0
-            let g:neovide_scale_factor = 1.0
-          else
-            let g:neovide_scale_factor = g:neovide_scale_factor * a:delta
-          endif
-        endfunction
-        nnoremap <expr><C-=> ChangeScaleFactor(1.25)
-        nnoremap <expr><C--> ChangeScaleFactor(1/1.25)
-        nnoremap <expr><C-BS> ChangeScaleFactor(0)
-      endif
-    '';
+    extraConfig = builtins.readFile ./rc.vim;
   };
 }
