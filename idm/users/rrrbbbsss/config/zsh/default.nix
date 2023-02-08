d56c80dcaa413a745c1317eb12121edd7163dbfd
Royce Strange <rrrbbbsss@ryzst.net>
2023-02-07 20:41:04 -0600

    zsh prepend sudo

--- idm/users/rrrbbbsss/config/zsh/default.nix
+++ idm/users/rrrbbbsss/config/zsh/default.nix
@@ -6,13 +6,13 @@
     defaultKeymap = "viins";
     enableCompletion = true;
     enableSyntaxHighlighting = true;
-    enableVteIntegration = true;
     loginExtra = ''
       if [ -z "''${DISPLAY}" ] && [ "''${XDG_VTNR}" -eq 1 ]; then
         exec sway
       fi
     '';
     initExtra = ''
+      stty -ixon
       source ${pkgs.zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh
       source ${pkgs.zsh-vi-mode}/share/zsh-vi-mode/zsh-vi-mode.plugin.zsh
       autoload -Uz vcs_info
@@ -22,6 +22,13 @@
       PSPACE='%K{#4e4e4e} %k'
       PROMPT='$PSPACE%(?.%K{green} %k.%K{red} %k)$PSPACE%K{#000000} %n $PSPACE%K{#000000} %m $PSPACE%K{#000000} %40<...<%~/%<< $PSPACE''${vcs_info_msg_0_}%k 
       $ '
+      function prepend_sudo() {
+        if [[ $BUFFER != "sudo "* ]]; then
+          BUFFER="sudo $BUFFER"; CURSOR+=5
+        fi
+      }
+      zle -N prepend_sudo
+      bindkey -M vicmd "\C-s" prepend_sudo
     '';
     shellAliases = {
       "-s pdf" = "zathura --fork";
