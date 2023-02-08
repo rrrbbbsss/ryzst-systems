a8dad1c8c44bc961ec828798bcd9ad3005b03a5d
Royce Strange <rrrbbbsss@ryzst.net>
2023-02-07 21:23:43 -0600

    zsh show vi mode in prompt

--- idm/users/rrrbbbsss/config/zsh/default.nix
+++ idm/users/rrrbbbsss/config/zsh/default.nix
@@ -18,10 +18,17 @@
       autoload -Uz vcs_info
       precmd() { vcs_info }
       zstyle ':vcs_info:git:*' formats '%K{#000000} %b %K{#4e4e4e} %k'
+      function zle-keymap-select {
+        VIMODE="''${''${KEYMAP/vicmd/$}/(main|viins)/>}"
+        zle reset-prompt
+      }
+      zle -N zle-keymap-select
       setopt PROMPT_SUBST
       PSPACE='%K{#4e4e4e} %k'
+      VIMODE='>'
       PROMPT='$PSPACE%(?.%K{green} %k.%K{red} %k)$PSPACE%K{#000000} %n $PSPACE%K{#000000} %m $PSPACE%K{#000000} %40<...<%~/%<< $PSPACE''${vcs_info_msg_0_}%k 
-      $ '
+      $VIMODE '
+
       function prepend_sudo() {
         if [[ $BUFFER != "sudo "* ]]; then
           BUFFER="sudo $BUFFER"; CURSOR+=5
