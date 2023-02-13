7207130d42f614b60130b3b07b0426b0cff6e798
Royce Strange <rrrbbbsss@ryzst.net>
2023-02-13 17:01:54 -0600

    vscode add some extensions

--- idm/users/rrrbbbsss/config/vscode/default.nix
+++ idm/users/rrrbbbsss/config/vscode/default.nix
@@ -1,25 +1,50 @@
 { pkgs, ... }:
 {
-    programs.vscode = {
-      enable = true;
-      #enableUpdateCheck = false;
-      #enableExtensionUpdateCheck = false;
-      package = pkgs.vscodium;
-      extensions = [
-        # vim keys
-        pkgs.vscode-extensions.vscodevim.vim
-        # dir env
-        pkgs.vscode-extensions.mkhl.direnv
-        # nix
-        pkgs.vscode-extensions.jnoortheen.nix-ide
-        # rust
-        pkgs.vscode-extensions.matklad.rust-analyzer
-        pkgs.vscode-extensions.tamasfe.even-better-toml
-        # python
-        pkgs.vscode-extensions.ms-python.python
-        # org-mode
-        #(import vscode-org-mode.nix)
-        # will have to package myself...
-      ];
+  programs.vscode = {
+    enable = true;
+    enableUpdateCheck = false;
+    package = pkgs.vscodium;
+    userSettings = {
+      "nix.enableLanguageServer" = true;
+      "nix.serverPath" = "nil";
+      "nix.serverSettings" = {
+        "nil" = {
+          "formatting" = {
+            "command" = [
+              "nixpkgs-fmt"
+            ];
+          };
+        };
+      };
+      "explorer.confirmDragAndDrop" = false;
     };
+    extensions = with pkgs.vscode-extensions; [
+      # vim keys
+      vscodevim.vim
+      # dir env
+      mkhl.direnv
+      # nix
+      jnoortheen.nix-ide
+      # rust
+      matklad.rust-analyzer
+      tamasfe.even-better-toml
+      # python
+      ms-python.python
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      # prolog
+      {
+        publisher = "arthurwang";
+        name = "vsc-prolog";
+        version = "0.8.23";
+        sha256 = "sha256-Da2dCpruVqzP3g1hH0+TyvvEa1wEwGXgvcmIq9B/2cQ=";
+      }
+      # org
+      {
+        publisher = "tootone";
+        name = "org-mode";
+        version = "0.5.0";
+        sha256 = "sha256-vXwo3oFLwK/wY7XEph9lGvXYIxjZsxeIE4TVAROmV2o=";
+      }
+    ];
+  };
 }
