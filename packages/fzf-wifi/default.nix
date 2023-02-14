05898800467d9cfbbfae445ebadd702fb2f6a254
Royce Strange <rrrbbbsss@ryzst.net>
2023-02-14 00:19:06 -0600

    tweak fzf-wifi

--- packages/fzf-wifi/default.nix
+++ packages/fzf-wifi/default.nix
@@ -1,8 +1,8 @@
-{ lib, runCommandLocal, makeWrapper, bash, pass-wayland, fzf }:
+{ lib, runCommandLocal, makeWrapper, bash, networkmanager, fzf }:
 runCommandLocal "fzf-wifi" {
   script = ./fzf-wifi.sh;
   nativeBuildInputs = [ makeWrapper ];
 } ''
   makeWrapper $script $out/bin/fzf-wifi \
-  --prefix PATH : ${lib.makeBinPath [ bash pass-wayland fzf]}
+  --prefix PATH : ${lib.makeBinPath [ bash networkmanager fzf]}
 ''
