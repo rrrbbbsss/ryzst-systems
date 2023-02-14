045b89e45929e81fe1884d9690ec45421dd61606
Royce Strange <rrrbbbsss@ryzst.net>
2023-02-13 22:39:17 -0600

    tweak fzf-wifi

--- packages/fzf-wifi/fzf-wifi.sh
+++ packages/fzf-wifi/fzf-wifi.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+echo "Scanning..."
+
 fields="BSSID,SSID,BARS,SECURITY,IN-USE"
 ssids=$(nmcli -f $fields dev wifi list | tail -n +2)
 selection=$(printf '%s\n' "${ssids[@]}" | fzf --reverse --prompt='wifi > ' )
+clear
 
 [[ -n $selection ]] || exit
 
