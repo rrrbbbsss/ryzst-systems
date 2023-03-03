66f5dc96b512fdc4591945629549f2887e2c29fd
Royce Strange <rrrbbbsss@ryzst.net>
2023-03-03 01:24:58 -0600

    more laptop

--- idm/users/rrrbbbsss/config/gpg/default.nix
+++ idm/users/rrrbbbsss/config/gpg/default.nix
@@ -3,7 +3,7 @@ rec {
   programs.gpg = {
     enable = true;
     package = pkgs.gnupg;
-    # publicKeys = [ { source = ./pubkeys.txt; trust = ultimate } ];
+    publicKeys = [ { source = ../../pubkeys/gpg.pub; trust = "ultimate"; } ];
     scdaemonSettings = {
       disable-ccid = true;
     };
