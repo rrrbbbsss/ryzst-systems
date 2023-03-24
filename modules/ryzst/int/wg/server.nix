f07c510cf1092298edf51f77bfd1466142594f48
Royce Strange <rrrbbbsss@ryzst.net>
2023-03-24 11:59:48 -0500

    update flake

--- modules/ryzst/int/wg/server.nix
+++ modules/ryzst/int/wg/server.nix
@@ -1,12 +1,6 @@
 { config, pkgs, lib, ... }:
 with lib;
 let
-  # todo: remove after flake update
-  foldlAttrs = f: init: set:
-    foldl'
-      (acc: name: f acc name set.${name})
-      init
-      (attrNames set);
   # todo: get from "deployment" mapping
   server.ip = "10.255.255.1";
   server.subnet = "10.255.255.0/24";
@@ -14,7 +8,7 @@ let
   server.publicKey = "25o3/I0MWQJMzEJ9+6ipCFaSZtCkETblvhGnivDu0Q0=";
   server.endpoint = "192.168.0.1";
 
-  clients = foldlAttrs
+  clients = attrsets.foldlAttrs
     (acc: n: v:
       [{ publicKey = v.keys.wg0; allowedIPs = [ "${v.ip}/32" ]; }] ++ acc)
     [ ]
--- modules/ryzst/mek/default.nix
+++ modules/ryzst/mek/default.nix
@@ -1,19 +1,13 @@
 { config, pkgs, lib, ... }:
 with lib;
 let
-  # todo: remove after flake update
-  foldlAttrs = f: init: set:
-    foldl'
-      (acc: name: f acc name set.${name})
-      init
-      (attrNames set);
   hosts = with builtins;
     let dir = ../../../hosts; in
     mapAttrs (n: v: fromJSON (readFile (dir + "/${n}/registration.json")))
       (readDir dir);
   hostsFile = with builtins;
     toFile "${config.networking.domain}-hostsFile"
-      (foldlAttrs
+      (attrsets.foldlAttrs
         (acc: n: v: "${v.ip} ${n}.${config.networking.domain}\n${acc}") ""
         hosts);
 in
