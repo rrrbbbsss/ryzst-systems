37e75c0e9bbebb221d65b68778299e978acfd812
Royce Strange <rrrbbbsss@ryzst.net>
2023-04-30 06:42:55 -0500

    katago-model cleanup

--- packages/katago-model/default.nix
+++ packages/katago-model/default.nix
@@ -1,4 +1,4 @@
-{ stdenv, lib, fetchurl, qemu, bash, ... }:
+{ stdenvNoCC, lib, fetchurl, ... }:
 
 let
   pname = "katago-model";
@@ -6,14 +6,13 @@ let
   name = "${pname}-${version}.bin.gz";
 in
 
-stdenv.mkDerivation {
+stdenvNoCC.mkDerivation {
   inherit pname version name;
   src = fetchurl {
     url = "https://media.katagotraining.org/uploaded/networks/models/kata1/kata1-b40c256-s11840935168-d2898845681.bin.gz";
     sha256 = "4179c907f5e8850ff3fa9d1d2d4e590ef7d1643ab4d4bd7324e6519c8d0562bc";
   };
   phases = [ "installPhase" ];
-  buildInputs = [ qemu ];
   installPhase = ''
     cp $src $out
   '';
