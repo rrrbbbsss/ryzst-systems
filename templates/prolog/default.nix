369757e0bc78e0e13c56fab8b44f35e08a26519f
Royce Strange <rrrbbbsss@ryzst.net>
2023-06-20 22:38:03 -0500

    templates: basic template to start learning prolog

---
+++ templates/prolog/default.nix
@@ -0,0 +1,3 @@
+{
+  description = "basic prolog template";
+}
---
+++ templates/prolog/files/flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "todo";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell
+        {
+          nativeBuildInputs = with pkgs; [
+            scryer-prolog
+          ];
+        };
+    };
+}
