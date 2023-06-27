9298a3c6d6c600a40f5fb0bacf6a0a9aa9d08ab2
Royce Strange <rrrbbbsss@ryzst.net>
2023-06-27 17:11:06 -0500

    firefox: manage ublock

--- idm/users/rrrbbbsss/default.nix
+++ idm/users/rrrbbbsss/default.nix
@@ -35,6 +35,7 @@
       ../../../modules/home
       ./config/alacritty
       ./config/emacs
+      ./config/firefox
       ./config/gpg
       ./config/mpv
       ./config/nvim
@@ -82,50 +83,6 @@
       };
     };
 
-    programs.firefox = {
-      enable = true;
-      profiles.default = {
-        id = 0;
-        isDefault = true;
-        settings = {
-          "browser.startup.homepage" = "https://google.com";
-          "browser.newtabpage.enabled" = false;
-          "media.ffmpeg.vaapi.enabled" = true;
-          "extensions.pocket.enabled" = false;
-          "extensions.autoDisableScopes" = 14;
-          "signon.rememberSignons" = false;
-          "network.IDN_show_punycode" = true;
-          "geo.enabled" = false;
-          "datareporting.healthreport.uploadEnabled" = false;
-          "privacy.firstparty.isolate" = true;
-          "privacy.resistFingerprinting" = false;
-          "browser.cache.offline.enable" = false;
-          "dom.battery.enabled" = false;
-          "dom.event.clipboardevents.enabled" = false;
-          "network.trr.mode" = 5;
-          "dom.security.https_only_mode" = true;
-          "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
-          "browser.urlbar.suggest.quicksuggest.sponsored" = false;
-          "extensions.formautofill.addresses.enabled" = false;
-          "extensions.formautofill.creditCards.enabled" = false;
-          "privacy.clearOnShutdown.offlineApps" = true;
-          "privacy.clearOnShutdown.cookies" = true;
-          "layout.css.prefers-color-scheme.content-override" = 0;
-        };
-        search = {
-          force = true;
-          engines = {
-            "Bing".metaData.hidden = true;
-            "Amazon.com".metaData.hidden = true;
-            "eBay".metaData.hidden = true;
-          };
-        };
-        extensions = with pkgs.firefox-addons; [
-          ublock-origin
-          tridactyl
-        ];
-      };
-    };
 
     programs.obs-studio = {
       enable = true;
