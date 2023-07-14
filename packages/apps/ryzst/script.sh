f6804f9c19abdef3c9d819de969d0b23e6c1c9ed
Royce Strange <rrrbbbsss@ryzst.net>
2023-07-14 12:27:14 -0500

    apps: write default app picker

--- packages/apps/ryzst/script.sh
+++ packages/apps/ryzst/script.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
-set -eo pipefail
+set -euo pipefail
 
-echo "TODO"
+flake="github:rrrbbbsss/ryzst-systems"
+nix='nix --option experimental-features "nix-command flakes"'
+system=$(nix eval --impure --raw --expr 'builtins.currentSystem')
+apps=$(nix flake show "$flake" --json 2>/dev/null \
+	   | jq '.apps."'$system'" | del(.default)')
+choices=$(jq -r 'keys[]' <<< "$apps")
+selection=$(fzf --reverse --prompt 'Select App > ' <<< "$choices")
+
+nix run "$flake"\#"$selection"
