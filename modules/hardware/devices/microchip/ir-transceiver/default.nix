21ca855180cabdc77efd0594027a899360c46bb8
Royce Strange <rrrbbbsss@ryzst.net>
2023-07-28 02:39:04 -0500

    hardware: add keybind for tv powerbutton

--- modules/hardware/devices/microchip/ir-transceiver/default.nix
+++ modules/hardware/devices/microchip/ir-transceiver/default.nix
@@ -1,6 +1,20 @@
 { config, pkgs, ... }:
+let
+  inherit (config.device) user;
+  inherit (config.home-manager.users.${user}.wayland.windowManager.sway.config) modifier;
+  group = "lirc";
+in
 {
-  environment.systemPackages = with pkgs; [
-    v4l-utils
-  ];
+  users.groups.${group} = { };
+  users.users.${user}.extraGroups = [ group ];
+  services.udev.extraRules = ''
+    KERNEL=="lirc[0-9]*", SUBSYSTEM=="lirc", \
+    SUBSYSTEMS=="usb", ATTRS{idVendor}=="04d8", ATTRS{idProduct}=="fd08", \
+    GROUP="${group}", MODE="0660"
+  '';
+  home-manager.users.${user}.wayland.windowManager.sway.config.keybindings = {
+    "${modifier}+Prior" = ''
+      exec ${pkgs.v4l-utils}/bin/ir-ctl -d /dev/lirc0 --send=${./powerbutton}
+    '';
+  };
 }
