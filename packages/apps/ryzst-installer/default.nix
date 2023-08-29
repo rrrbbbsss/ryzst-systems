df490e2a6f9e31c338ae11f455f3a83312690b27
Royce Strange <rrrbbbsss@ryzst.net>
2023-08-28 20:34:52 -0500

    apps: update installer inputs

--- packages/apps/ryzst-installer/default.nix
+++ packages/apps/ryzst-installer/default.nix
@@ -1,9 +1,50 @@
-{ lib, runCommandLocal, makeWrapper, bash, fzf, coreutils-full, util-linux, gawk, ryzst }:
+{ lib
+, runCommandLocal
+, makeWrapper
+, bash
+, fzf
+, coreutils-full
+, util-linux
+, gawk
+, ryzst
+, openssl
+, wireguard-tools
+, jq
+, gnupg
+, gnused
+, gnugrep
+, git
+, zfs
+, systemd
+, nix
+, openssh
+, networkmanager
+, curl
+}:
+
 runCommandLocal "ryzst-installer"
 {
   script = ./script.sh;
   nativeBuildInputs = [ makeWrapper ];
-} ''
-  makeWrapper $script $out/bin/ryzst-installer \
-  --prefix PATH : ${lib.makeBinPath [bash fzf coreutils-full util-linux gawk ryzst.fzf-wifi]}
-''
+} ''makeWrapper $script $out/bin/ryzst-installer \
+      --prefix PATH : ${lib.makeBinPath [
+  bash
+  fzf
+  coreutils-full
+  util-linux
+  gawk
+  ryzst.fzf-wifi
+  openssl
+  wireguard-tools
+  jq
+  gnupg
+  gnused
+  gnugrep
+  git
+  zfs
+  systemd
+  nix
+  openssh
+  networkmanager
+  curl
+  ]}''
