9951d7721142fc3a2ee737e665a2dce87d6b554e
Royce Strange <rrrbbbsss@ryzst.net>
2023-08-29 01:57:49 -0500

    overlays: keep tweaks in one overlay

--- overlays/default.nix
+++ overlays/default.nix
@@ -5,18 +5,16 @@ let
     firefox-addons = self.inputs.firefox-addons.packages.${prev.system};
     lib = prev.lib // { ryzst = self.lib; };
   };
-  good-old-packages = import ./good-old-packages.nix;
-  new-fixes = import ./new-fixes.nix;
+  tweaks = import ./tweaks.nix;
 in
 {
-  inherit ryzst good-old-packages;
+  inherit ryzst tweaks;
   default =
     self.inputs.nixpkgs.lib.composeManyExtensions
       [
         ryzst
         self.inputs.nix-vscode-extensions.overlays.default
         self.inputs.emacs-overlay.overlays.default
-        good-old-packages
-        new-fixes
+        tweaks
       ];
 }
