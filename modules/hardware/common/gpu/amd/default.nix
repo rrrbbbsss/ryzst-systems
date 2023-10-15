2cc1f0e08949c8a9ac8365423c6cc70c85c9a7db
Royce Strange <rrrbbbsss@ryzst.net>
2023-10-15 16:34:33 -0500

    flake: update

--- modules/hardware/common/gpu/amd/default.nix
+++ modules/hardware/common/gpu/amd/default.nix
@@ -7,12 +7,15 @@
     driSupport32Bit = true;
     extraPackages = with pkgs; [
       amdvlk
-      glxinfo
-      rocm-opencl-icd
-      rocm-opencl-runtime
-      mesa.drivers
-      rocminfo
+      rocmPackages.clr
+      rocmPackages.clr.icd
     ];
   };
-  environment.variables.AMD_VULKAN_ICD = lib.mkDefault "RADV";
+  environment = {
+    variables.AMD_VULKAN_ICD = lib.mkDefault "RADV";
+    systemPackages = with pkgs; [
+      glxinfo
+      rocmPackages.rocminfo
+    ];
+  };
 }
