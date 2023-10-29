43607b2a2667b72ad49cb37b9242cca7da05fc63
Royce Strange <rrrbbbsss@ryzst.net>
2023-10-29 14:56:11 -0500

    firefox: fix deprecation warning

--- idm/users/rrrbbbsss/config/firefox/default.nix
+++ idm/users/rrrbbbsss/config/firefox/default.nix
@@ -4,7 +4,7 @@
   programs.firefox = {
     enable = true;
     package = pkgs.firefox.override {
-      cfg = { enableTridactylNative = true; };
+      nativeMessagingHosts = [ pkgs.tridactyl-native ];
     };
     profiles.default = {
       id = 0;
