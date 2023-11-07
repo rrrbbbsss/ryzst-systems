f1a57cfd2af3be9fea97aed60180ba8dba1b69d4
Royce Strange <rrrbbbsss@ryzst.net>
2023-11-06 18:23:05 -0600

    overlays: hyprpicker released version with fix

--- overlays/tweaks.nix
+++ overlays/tweaks.nix
@@ -1,15 +1,5 @@
 final: prev:
 {
-  #https://github.com/hyprwm/hyprpicker/issues/35
-  hyprpicker = prev.hyprpicker.overrideAttrs (old: {
-    src = prev.fetchFromGitHub {
-      owner = "hyprwm";
-      repo = "hyprpicker";
-      rev = "5ba32686943f839d072426d9ffd172decaee0e3e";
-      hash = "sha256-3n0HJgo+3YCuo56a+efzcrh5UsfXi5jPU4tjqzJVm7g=";
-    };
-  });
-
   ryzst = prev.ryzst // {
     overrides.emacs = epkgs: epkgs // { };
   };
