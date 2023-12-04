f9e0beb1276802faa288f02f12b159980ef5a269
Royce Strange <rrrbbbsss@ryzst.net>
2023-12-03 22:48:23 -0600

    emacs: fix doom-themes

--- overlays/tweaks.nix
+++ overlays/tweaks.nix
@@ -1,6 +1,17 @@
 final: prev:
 {
   ryzst = prev.ryzst // {
-    overrides.emacs = epkgs: epkgs // { };
+    overrides.emacs = epkgs: epkgs // {
+      #https://github.com/Alexander-Miller/treemacs/issues/1073
+      doom-themes = epkgs.melpaPackages.doom-themes.overrideAttrs
+        (old: {
+          patches = [
+            (prev.fetchpatch {
+              url = "https://github.com/doomemacs/themes/pull/811.patch";
+              sha256 = "sha256-dDzVtfWCaa+fC9yHMKrwD5kn+xo0oq4KdASa3GTKJIs=";
+            })
+          ];
+        });
+    };
   };
 }
