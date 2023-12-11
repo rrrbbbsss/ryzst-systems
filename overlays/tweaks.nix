1a5fadd9ea0058f71d12bee28f5e8f8ab305036e
Royce Strange <rrrbbbsss@ryzst.net>
2023-12-10 22:09:32 -0600

    flake: update

--- overlays/tweaks.nix
+++ overlays/tweaks.nix
@@ -1,17 +1,6 @@
 final: prev:
 {
   ryzst = prev.ryzst // {
-    overrides.emacs = epkgs: epkgs // {
-      #https://github.com/doomemacs/themes/issues/809
-      doom-themes = epkgs.melpaPackages.doom-themes.overrideAttrs
-        (old: {
-          patches = [
-            (prev.fetchpatch {
-              url = "https://github.com/doomemacs/themes/pull/811.patch";
-              sha256 = "sha256-dDzVtfWCaa+fC9yHMKrwD5kn+xo0oq4KdASa3GTKJIs=";
-            })
-          ];
-        });
-    };
+    overrides.emacs = epkgs: epkgs // { };
   };
 }
