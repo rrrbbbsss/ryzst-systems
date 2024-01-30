3e3762866b50b9c17f5e81d7b4a1de34fbf8ba4f
Royce Strange <rrrbbbsss@ryzst.net>
2024-01-29 23:02:03 -0600

    lib: move mkHosts out and cleanup

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,3 +1,17 @@
 { self, ... }:
-
-self.lib.mkHosts ../hosts
+let
+  mkHosts = dir: with builtins;
+    mapAttrs
+      (name: path:
+        self.inputs.nixpkgs.lib.nixosSystem
+          {
+            modules = [
+              self.outputs.nixosModules.default
+              { os.hostname = name; }
+              (path + "/default.nix")
+              (path + "/hardware.nix")
+            ];
+          })
+      (self.lib.getDirs dir);
+in
+mkHosts ../hosts
--- lib/default.nix
+++ lib/default.nix
@@ -1,50 +1,25 @@
 { self, ... }:
 let
   lib-nixpkgs = self.inputs.nixpkgs.lib;
+
   getDirs = dir: with lib-nixpkgs.attrsets;
     foldlAttrs
       (acc: n: v:
         if v == "directory" then { ${n} = dir + "/${n}"; } // acc else acc)
       { }
       (builtins.readDir dir);
+
   getFilesList = dir: with lib-nixpkgs.attrsets;
     foldlAttrs
       (acc: n: v:
         if v == "regular" then [ (dir + "/${n}") ] ++ acc else acc)
       [ ]
       (builtins.readDir dir);
-  mkSystem = { name, path, target }:
-    let
-      hardwares =
-        {
-          host = [ (path + "/hardware.nix") ];
-          vm = [ (path + "/vm.nix") ];
-          iso = [ ];
-        };
-    in
-    lib-nixpkgs.nixosSystem {
-      modules = [
-        self.outputs.nixosModules.default
-        { os.hostname = name; }
-        (path + "/default.nix")
-      ] ++ hardwares.${target};
-    };
-
-  mkSystems = { dir, target }: with builtins;
-    mapAttrs
-      (name: value:
-        mkSystem { inherit name target; path = dir + "/${name}"; })
-      (getDirs dir);
-
-  mkHosts = dir: mkSystems { inherit dir; target = "host"; };
 
   hostnames = import ./names { inherit self; };
-
-  lib = {
-    inherit getDirs;
-    inherit getFilesList;
-    inherit mkHosts;
-    inherit hostnames;
-  };
 in
-lib
+{
+  inherit getDirs;
+  inherit getFilesList;
+  inherit hostnames;
+}
