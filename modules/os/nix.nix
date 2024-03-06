32f010162eb0da201199cadb33f91bc259daca13
Royce Strange <rrrbbbsss@ryzst.net>
2024-03-06 08:18:38 -0600

    modules: move emacs-overlay settings

--- modules/os/nix.nix
+++ modules/os/nix.nix
@@ -23,12 +23,6 @@ in
           experimental-features = [ "nix-command" "flakes" ];
           auto-optimise-store = true;
           flake-registry = "";
-          trusted-public-keys = [
-            "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-          ];
-          substituters = [
-            "https://nix-community.cachix.org"
-          ];
         };
         extraOptions = ''
           keep-outputs = true
