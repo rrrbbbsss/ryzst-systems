5230914f9af0d8ec9e2022b7e452d2e00d02d74b
Royce Strange <rrrbbbsss@ryzst.net>
2024-03-17 19:28:37 -0500

    media: update firefox settings

--- hosts/mud-orb/default.nix
+++ hosts/mud-orb/default.nix
@@ -62,7 +62,38 @@ in
         settings = {
           "browser.startup.homepage" = "https://google.com";
           "browser.newtabpage.enabled" = false;
+          "media.ffmpeg.vaapi.enabled" = true;
           "extensions.pocket.enabled" = false;
+          "extensions.autoDisableScopes" = 14;
+          "signon.rememberSignons" = false;
+          "network.IDN_show_punycode" = true;
+          "geo.enabled" = false;
+          "datareporting.healthreport.uploadEnabled" = false;
+          "privacy.firstparty.isolate" = true;
+          "privacy.resistFingerprinting" = false;
+          "privacy.trackingprotection.enabled" = true;
+          "browser.cache.offline.enable" = false;
+          "dom.battery.enabled" = false;
+          "dom.event.clipboardevents.enabled" = false;
+          "network.trr.mode" = 5;
+          "dom.security.https_only_mode" = true;
+          "browser.urlbar.suggest.quicksuggest.nonsponsored" = false;
+          "browser.urlbar.suggest.quicksuggest.sponsored" = false;
+          "extensions.formautofill.addresses.enabled" = false;
+          "extensions.formautofill.creditCards.enabled" = false;
+          "privacy.clearOnShutdown.offlineApps" = true;
+          "privacy.clearOnShutdown.cookies" = true;
+          "layout.css.prefers-color-scheme.content-override" = 0;
+          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+          "browser.tabs.inTitlebar" = 0;
+        };
+        search = {
+          force = true;
+          engines = {
+            "Bing".metaData.hidden = true;
+            "Amazon.com".metaData.hidden = true;
+            "eBay".metaData.hidden = true;
+          };
         };
         extensions = with pkgs.firefox-addons; [
           ublock-origin
