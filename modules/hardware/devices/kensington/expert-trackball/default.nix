8da12c877a32f8bfa1b66ab11490daf819ff9424
Royce Strange <rrrbbbsss@ryzst.net>
2024-05-06 00:19:46 -0500

    hardware: enable scroll_button_lock

--- modules/hardware/devices/kensington/expert-trackball/default.nix
+++ modules/hardware/devices/kensington/expert-trackball/default.nix
@@ -3,6 +3,7 @@
   device.rats = {
     "1149:4128:Kensington_Expert_Mouse" = {
       scroll_button = "BTN_SIDE";
+      scroll_button_lock = "enabled";
       scroll_method = "on_button_down";
       natural_scroll = "enabled";
     };
