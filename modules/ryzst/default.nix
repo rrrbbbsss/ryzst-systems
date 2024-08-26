c36a12095e73e5f28787347c61542148279ac0c7
Royce Strange <rrrbbbsss@ryzst.net>
2024-08-26 01:04:24 -0500

    domain: cleanup syntax.

--- modules/ryzst/default.nix
+++ modules/ryzst/default.nix
@@ -7,78 +7,78 @@ in
 
   ryzst.int = {
     cache = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
-        inherit cat-jar ape-orc car-fan mud-orb ant-bot;
+      client.nodes = {
+        inherit (hosts) cat-jar ape-orc car-fan mud-orb ant-bot;
       };
     };
 
     dns = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
-        inherit cat-jar ape-orc car-fan mud-orb ant-bot;
+      client.nodes = {
+        inherit (hosts) cat-jar ape-orc car-fan mud-orb ant-bot;
       };
     };
 
     git = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
-        inherit cat-jar ape-orc car-fan;
+      client.nodes = {
+        inherit (hosts) cat-jar ape-orc car-fan;
       };
     };
 
     nfs = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
-        inherit cat-jar ape-orc;
+      client.nodes = {
+        inherit (hosts) cat-jar ape-orc;
       };
     };
 
     ntp = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
-        inherit cat-jar ape-orc car-fan mud-orb ant-bot;
+      client.nodes = {
+        inherit (hosts) cat-jar ape-orc car-fan mud-orb ant-bot;
       };
     };
 
     remote-build = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
+      client.nodes = {
         # TODO: remove ape-orc when ci is in place
-        inherit ape-orc car-fan;
+        inherit (hosts) ape-orc car-fan;
       };
     };
 
     syncthing = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
-        inherit ape-orc cat-jar car-fan;
+      client.nodes = {
+        inherit (hosts) ape-orc cat-jar car-fan;
       };
-      reading.nodes = with hosts; {
-        inherit gas-ink;
+      reading.nodes = {
+        inherit (hosts) gas-ink;
       };
     };
 
     wg = {
-      server.nodes = with hosts; {
-        inherit tin-jet;
+      server.nodes = {
+        inherit (hosts) tin-jet;
       };
-      client.nodes = with hosts; {
-        inherit cat-jar ape-orc car-fan mud-orb ant-bot gas-ink;
+      client.nodes = {
+        inherit (hosts) cat-jar ape-orc car-fan mud-orb ant-bot gas-ink;
       };
     };
   };
