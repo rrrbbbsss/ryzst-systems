55d21e16987e9d8a2180c92e501cf067d16786e4
Royce Strange <rrrbbbsss@ryzst.net>
2024-09-19 03:14:14 -0500

    emacs: set default-input-method

--- idm/users/man/config/emacs/init.el
+++ idm/users/man/config/emacs/init.el
@@ -27,6 +27,7 @@
 (defalias 'yes-or-no-p 'y-or-n-p)
 (global-auto-revert-mode t)
 (setq native-comp-async-report-warnings-errors nil)
+(setq default-input-method "TeX")
 
 ;; sticky buffers
 (customize-set-variable
