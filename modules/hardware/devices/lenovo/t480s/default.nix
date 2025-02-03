8dc1e520a7b175a00b8885082aa5dd68c89b18d8
Royce Strange <rrrbbbsss@ryzst.net>
2025-02-02 19:18:30 -0600

    hardware: add lenovo t480s.

---
+++ modules/hardware/devices/lenovo/t480s/default.nix
@@ -0,0 +1,34 @@
+{ ... }:
+{
+  imports = [
+    ../../../common/cpu/intel
+    ../../../common/gpu/intel
+    ../../../common/tpm/2-0
+  ];
+
+  services.throttled.enable = true;
+
+  device.monitors = {
+    eDP-1 = {
+      number = "0";
+      mode = "1920x1080@60Hz";
+    };
+    HDMI-2 = {
+      number = "1";
+    };
+  };
+
+  device.rats = {
+    "1267:32:Elan_TrackPoint" = {
+      natural_scroll = "disabled";
+      accel_profile = "adaptive";
+      pointer_accel = "1";
+    };
+  };
+
+  device.keyboard = {
+    remap.enable = true;
+    name = "lenovo-t480s";
+    path = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
+  };
+}
