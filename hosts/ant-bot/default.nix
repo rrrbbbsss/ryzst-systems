1988bcc2fa8870014237bce216630a7282d315c0
Royce Strange <rrrbbbsss@ryzst.net>
2025-05-22 20:07:34 -0500

    hosts: cleanup ant-bot.

--- hosts/ant-bot/default.nix
+++ hosts/ant-bot/default.nix
@@ -1,47 +1,4 @@
-{ lib, ... }:
-let
-  wifi = "wlan0";
-  lan = "end0";
-  bridge = "br0";
-in
+{ ... }:
 {
   imports = [ ];
-
-  # TODO: redo
-  services.hostapd = {
-    enable = false;
-  };
-
-  networking.useDHCP = false;
-  services.resolved.enable = lib.mkForce false;
-  systemd.network = {
-    enable = true;
-    netdevs = {
-      ${bridge} = {
-        netdevConfig = {
-          Name = bridge;
-          Kind = "bridge";
-        };
-      };
-    };
-    networks = {
-      ${lan} = {
-        matchConfig = {
-          Name = lan;
-        };
-        networkConfig = {
-          Bridge = bridge;
-          DHCP = "no";
-        };
-      };
-      ${bridge} = {
-        matchConfig = {
-          Name = bridge;
-        };
-        networkConfig = {
-          DHCP = "ipv4";
-        };
-      };
-    };
-  };
 }
