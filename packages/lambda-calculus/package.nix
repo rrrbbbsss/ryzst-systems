2167d87155f1ab10f06d3960dd23668dc6dde295
Royce Strange <rrrbbbsss@ryzst.net>
2025-05-25 04:03:53 -0500

    packages: clean up lambda-calculus.

--- packages/lambda-calculus/package.nix
+++ packages/lambda-calculus/package.nix
@@ -1,8 +1,3 @@
-# https://www.youtube.com/watch?v=--1SS0bfUWg
-# yea, never really been able to have
-# a conversation with someone
-# about lambda-calculus...
-# life be like that.
 { writeScriptBin }:
 writeScriptBin "lambda-calculus"
   (builtins.readFile ./abstract-machine.pl)
