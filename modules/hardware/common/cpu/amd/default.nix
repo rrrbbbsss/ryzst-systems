a2c1a6acc80d2715ddb2344fd17b6c8b9a2c86a1
Royce Strange <rrrbbbsss@ryzst.net>
2025-06-04 07:14:12 -0500

    hardware: update note.

--- modules/hardware/common/cpu/amd/default.nix
+++ modules/hardware/common/cpu/amd/default.nix
@@ -1,10 +1,7 @@
-{ config, lib, ... }:
+{ lib, ... }:
 # https://www.youtube.com/watch?v=thmR2SmkeDU
 # my ryzen 7 5700G was having issues with random reboots/freezing.
 # had to disable "AMD Cool n' Quiet" in BIOS.
-# will have to see if it holds or not,
-# otherwise will have to poke through arch wiki:
-# https://wiki.archlinux.org/title/Ryzen#Troubleshooting
 {
   hardware.enableRedistributableFirmware = lib.mkDefault true;
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
