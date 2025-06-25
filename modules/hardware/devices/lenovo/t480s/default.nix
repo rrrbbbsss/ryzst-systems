065bafa7b7ef427066a16bb26af8725e998e053f
Royce Strange <rrrbbbsss@ryzst.net>
2025-06-24 23:09:20 -0500

    hardware: move some config to t480s.

--- modules/hardware/devices/lenovo/t480s/default.nix
+++ modules/hardware/devices/lenovo/t480s/default.nix
@@ -44,4 +44,68 @@
     name = "lenovo-t480s";
     path = "/dev/input/by-path/platform-i8042-serio-0-event-kbd";
   };
+
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  boot = {
+    loader = {
+      systemd-boot.enable = true;
+      efi = {
+        efiSysMountPoint = "/boot/efi";
+        canTouchEfiVariables = true;
+      };
+    };
+    tmp = {
+      useTmpfs = true;
+      tmpfsSize = "80%";
+    };
+    kernelParams = [ "console=tty1" ];
+    initrd.availableKernelModules = [
+      "xhci_pci"
+      "ahci"
+      "nvme"
+      "usbhid"
+      "usb_storage"
+      "sd_mod"
+    ];
+  };
+
+  zramSwap = {
+    enable = true;
+    algorithm = "zstd";
+  };
+
+  disko.devices = {
+    disk = {
+      sda = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            BOOT = {
+              label = "BOOT";
+              type = "EF00";
+              size = "512M";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountOptions = [ "umask=0077" ];
+                mountpoint = "/boot/efi";
+              };
+            };
+            ROOT = {
+              label = "ROOT";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
 }
