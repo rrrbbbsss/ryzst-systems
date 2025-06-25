080e73e8b82034035c348a6a1bbd51117f73fd8b
Royce Strange <rrrbbbsss@ryzst.net>
2025-06-25 16:37:03 -0500

    packages: tinker with hardware-scanner.

--- packages/hardware-scanner/script.sh
+++ packages/hardware-scanner/script.sh
@@ -1,6 +1,55 @@
 # shellcheck shell=bash
 
-# would like to scan for
-# only hardware in the catalog.
-# (keep output smol)
-echo "yoyoma"
+# https://wiki.archlinux.org/title/Modalias
+# https://pci-ids.ucw.cz/
+# http://www.linux-usb.org/usb-ids.html
+
+# so like if
+# modalias -> kernel module
+# so like then why not
+# modalias -> nixos module(?)
+# ...(?)...
+
+function resolve_modules() {
+  while read -r MODULE; do
+    modprobe --resolve-alias "$MODULE" 2>/dev/null || echo ""
+  done
+}
+
+MODALIASES=$(find /sys/devices -name modalias -print0 \
+               | xargs -0 cat \
+               | sort -u)
+printf 'modaliases:\n%s\n\n' "$MODALIASES"
+# displays
+# TODO: find a nice program to peel out info from edid mess
+#EDID=$(find /sys/devices -name edid -print0 \
+#               | xargs -0 cat \
+#               | sort -u)
+#printf 'edid:\n%s\n\n' "$EDID"
+
+# cpu
+CPU="todo"
+printf 'cpu:\n%s\n\n' "$CPU"
+# board
+BOARD=$(grep 'dmi:.*' <<<"$MODALIASES" | cut -d ':' -f 1,2)
+printf 'dmi:\n%s\n\n' "$BOARD"
+# graphics
+GRAPHICS=$(grep 'pci:v.*d.*sv.*sd.*bc03sc.*i.*' <<<"$MODALIASES")
+printf 'graphics:\n%s\n\n' "$GRAPHICS"
+# wireless (don't have good usb dingle to test/validate)
+WIRELESS=$(grep 'pci:v.*d.*sv.*sd.*bc02sc80i.*' <<<"$MODALIASES")
+printf 'wireless:\n%s\n\n' "$WIRELESS"
+# ethernet (don't have good usb dingle to test/validate)
+ETHERNET=$(grep 'pci:v.*d.*sv.*sd.*bc02sc00i.*' <<<"$MODALIASES")
+printf 'ethernet:\n%s\n\n' "$ETHERNET"
+# storage
+#STORAGE=TODO
+# tpm
+#TPM=todo
+
+#DEVICETREE=$(cat /sys/firmware/devicetree/base/compatible)
+#printf 'devicetree: %s' "$DEVICETREE"
+
+MODULES=$(resolve_modules <<<"$MODALIASES" \
+            | sort -u)
+printf 'modules:\n%s\n\n' "$MODULES"
