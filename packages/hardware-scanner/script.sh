a598b4564c099a1174d1d2a307f5f67eb633f4c2
Royce Strange <rrrbbbsss@ryzst.net>
2025-06-25 21:29:54 -0500

    packages: tinker with hardwaer-scanner.

--- packages/hardware-scanner/script.sh
+++ packages/hardware-scanner/script.sh
@@ -20,12 +20,14 @@ MODALIASES=$(find /sys/devices -name modalias -print0 \
                | xargs -0 cat \
                | sort -u)
 printf 'modaliases:\n%s\n\n' "$MODALIASES"
-# displays
-# TODO: find a nice program to peel out info from edid mess
-#EDID=$(find /sys/devices -name edid -print0 \
-#               | xargs -0 cat \
-#               | sort -u)
-#printf 'edid:\n%s\n\n' "$EDID"
+
+# https://uefi.org/PNP_ID_List
+# https://github.com/linuxhw/EDID
+# https://en.wikipedia.org/wiki/Extended_Display_Identification_Data#EDID_1.4_data_format
+EDID=$(find /sys/devices -name edid -print0 \
+               | xargs -0 cat \
+               | xxd -p -s+8 -l 4)
+printf 'edid:\n%s\n\n' "$EDID"
 
 # cpu
 CPU="todo"
