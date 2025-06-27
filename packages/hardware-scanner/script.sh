4751142461453080ca17817c663bd69c6f748059
Royce Strange <rrrbbbsss@ryzst.net>
2025-06-27 02:38:02 -0500

    packages: tinker with hardware-scanner.

--- packages/hardware-scanner/script.sh
+++ packages/hardware-scanner/script.sh
@@ -4,12 +4,6 @@
 # https://pci-ids.ucw.cz/
 # http://www.linux-usb.org/usb-ids.html
 
-# so like if
-# modalias -> kernel module
-# so like then why not
-# modalias -> nixos module(?)
-# ...(?)...
-
 function resolve_modules() {
   while read -r MODULE; do
     modprobe --resolve-alias "$MODULE" 2>/dev/null || echo ""
@@ -28,28 +22,8 @@ EDID=$(find /sys/devices -name edid -print0 \
                | xargs -0 -I '{}' xxd -p -s+8 -l 4 '{}')
 printf 'edid:\n%s\n\n' "$EDID"
 
-# cpu
-CPU="todo"
-printf 'cpu:\n%s\n\n' "$CPU"
-# board
-BOARD=$(grep 'dmi:.*' <<<"$MODALIASES" | cut -d ':' -f 1,2)
-printf 'dmi:\n%s\n\n' "$BOARD"
-# graphics
-GRAPHICS=$(grep 'pci:v.*d.*sv.*sd.*bc03sc.*i.*' <<<"$MODALIASES")
-printf 'graphics:\n%s\n\n' "$GRAPHICS"
-# wireless (don't have good usb dingle to test/validate)
-WIRELESS=$(grep 'pci:v.*d.*sv.*sd.*bc02sc80i.*' <<<"$MODALIASES")
-printf 'wireless:\n%s\n\n' "$WIRELESS"
-# ethernet (don't have good usb dingle to test/validate)
-ETHERNET=$(grep 'pci:v.*d.*sv.*sd.*bc02sc00i.*' <<<"$MODALIASES")
-printf 'ethernet:\n%s\n\n' "$ETHERNET"
 # storage
 #STORAGE=TODO
-# tpm
-#TPM=todo
-
-#DEVICETREE=$(cat /sys/firmware/devicetree/base/compatible)
-#printf 'devicetree: %s' "$DEVICETREE"
 
 MODULES=$(resolve_modules <<<"$MODALIASES" \
             | sort -u)
