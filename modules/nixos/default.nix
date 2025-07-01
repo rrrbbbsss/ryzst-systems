d405fcf42900a45d17e1ae4b86b60550a4c6d5c7
Royce Strange <rrrbbbsss@ryzst.net>
2025-07-01 03:56:49 -0500

    modules: add notes.

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -1,6 +1,7 @@
 self:
 let
   # TODO: make nicer later.
+  # (also make directory for each module and eventually add tests...)
   modulesList = self.lib.getFilesList ./modules;
   modules = self.lib.getFilesNoSuffix ./modules;
 in
