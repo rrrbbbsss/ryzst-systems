1d1782110dfde6b35aac3c4f11b27c790ab679f4
Royce Strange <rrrbbbsss@ryzst.net>
2025-07-02 07:21:56 -0500

    hosts: clean up ant-bot.

--- hosts/ant-bot/default.nix
+++ hosts/ant-bot/default.nix
@@ -1,8 +1,2 @@
 { ... }:
-{
-  imports = [
-    ../../modules/hardware/devices/brother/hl-l2300d
-    ../../modules/hardware/devices/canon/canoscan-lide-300
-    ../../modules/hardware/devices/creality/ender3-v3-se
-  ];
-}
+{ }
--- hosts/ant-bot/hardware.nix
+++ hosts/ant-bot/hardware.nix
@@ -3,5 +3,8 @@
   imports = [
     ../../modules/hardware/devices/raspberry-pi/4b
     ../../modules/hardware/devices/yubico/yubikey5
+    ../../modules/hardware/devices/brother/hl-l2300d
+    ../../modules/hardware/devices/canon/canoscan-lide-300
+    ../../modules/hardware/devices/creality/ender3-v3-se
   ];
 }
