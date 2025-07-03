df402416f0e1be77738cc9a75b22f61d0606c78e
Royce Strange <rrrbbbsss@ryzst.net>
2025-07-03 17:30:13 -0500

    domain: reformat file.

--- modules/ryzst/default.nix
+++ modules/ryzst/default.nix
@@ -12,76 +12,117 @@ in
   ryzst.int = {
     cache = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc mud-orb ant-bot;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc
+          mud-orb
+          ant-bot;
       };
     };
 
     ci = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client-rpc.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client-web.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc;
       };
     };
 
     cups = {
       server.nodes = {
-        inherit (hosts) ant-bot;
+        inherit (hosts)
+          ant-bot;
       };
       client.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc;
       };
     };
 
     dns = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc mud-orb ant-bot;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc
+          mud-orb
+          ant-bot;
       };
     };
 
     git = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client.nodes = {
-        inherit (hosts) tin-jet fox-cup cat-jar ape-orc mud-orb ant-bot;
+        inherit (hosts)
+          tin-jet
+          fox-cup
+          cat-jar
+          ape-orc
+          mud-orb
+          ant-bot;
       };
     };
 
     nfs = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client.nodes = {
-        inherit (hosts) cat-jar ape-orc;
+        inherit (hosts)
+          cat-jar
+          ape-orc;
       };
     };
 
     octoprint = {
       server.nodes = {
-        inherit (hosts) ant-bot;
+        inherit (hosts)
+          ant-bot;
       };
       client.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc;
       };
     };
 
     ntp = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc mud-orb ant-bot;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc
+          mud-orb
+          ant-bot;
       };
     };
 
@@ -92,31 +133,47 @@ in
 
     sane = {
       server.nodes = {
-        inherit (hosts) ant-bot;
+        inherit (hosts)
+          ant-bot;
       };
       client.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc;
       };
     };
 
     syncthing = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client.nodes = {
-        inherit (hosts) fox-cup ape-orc cat-jar;
+        inherit (hosts)
+          fox-cup
+          ape-orc
+          cat-jar;
       };
       reading.nodes = {
-        inherit (hosts) gas-ink;
+        inherit (hosts)
+          gas-ink;
       };
     };
 
     wg = {
       server.nodes = {
-        inherit (hosts) tin-jet;
+        inherit (hosts)
+          tin-jet;
       };
       client.nodes = {
-        inherit (hosts) fox-cup cat-jar ape-orc mud-orb ant-bot gas-ink;
+        inherit (hosts)
+          fox-cup
+          cat-jar
+          ape-orc
+          mud-orb
+          ant-bot
+          gas-ink;
       };
     };
   };
